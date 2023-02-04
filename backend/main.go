a8afef3e3d246d7efaf488d6c9c77fbd73ca9f58
Meyazhagan <meyazhagan.ofcl@gmail.com>
2023-02-04 22:37:17 +0530

    setup router

--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	fmt.Println("Deque Backend Server")
+
+	r := gin.Default()
+
+	r.GET("/", func(c *gin.Context) {
+		c.String(http.StatusOK, "Deque Backend Server Running...")
+	})
+
+	r.Run(":4000")
 }
